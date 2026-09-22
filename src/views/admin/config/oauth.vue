fa4a01e9dd2c9a54ac857d80a0b79ed898140a83
shanhe <net@shanhe.team>
2026-09-22 22:58:59 +0800

    refactor(oauth): 移除内置 OAuth 厂商类型，仅保留自定义 SSO
    - 删除 QQ / 微信 / Gitee / GitHub / 小程序 / Google / 公众号等 OAuth 类型常量与回调映射
    - 管理端 OAuth 配置页精简为仅自定义 OAuth 配置

--- src/views/admin/config/oauth.vue
+++ src/views/admin/config/oauth.vue
@@ -2,9 +2,6 @@
   <el-card shadow="never">
     <el-alert title="Oauth配置提示" type="success" class="oauth-tips">
       <div>当前Oauth的回调地址是：{{ origin }}/oauth/{{ oauthType }}</div>
-      <div>
-        公众号Token验证 URL地址：{{ origin }}/api/v1/oauth/officialaccount/token
-      </div>
     </el-alert>
     <el-tabs v-model="subActiveName" type="card" @tab-click="subTabClick">
       <el-tab-pane
@@ -12,21 +9,7 @@
         :key="'sub-category-' + item.value"
         :label="item.label"
         :name="item.value"
-      >
-        <el-alert
-          v-if="item.apply"
-          title="Oauth登录申请"
-          type="warning"
-          class="oauth-tips"
-        >
-          <a
-            :href="item.apply"
-            target="_blank"
-            class="el-link el-link--primary"
-            >{{ item.apply }}</a
-          >
-        </el-alert>
-      </el-tab-pane>
+      />
     </el-tabs>
     <FormConfig ref="formConfig" v-loading="loading" :init-configs="configs">
       <template #buttons>
@@ -70,28 +53,6 @@ import service from '@/utils/request'
 defineOptions({ name: 'AdminConfigOauth' })
 
 const subCategories = [
-  { label: '微信登录', value: 'oauthWechat', apply: 'https://open.weixin.qq.com/' },
-  {
-    label: '微信公众号',
-    value: 'oauthOfficialAccount',
-    apply: 'https://mp.weixin.qq.com/',
-  },
-  {
-    label: 'QQ登录',
-    value: 'oauthQQ',
-    apply: 'https://connect.qq.com/manage.html#/',
-  },
-  {
-    label: '谷歌登录',
-    value: 'oauthGoogle',
-    apply: 'https://console.developers.google.com/',
-  },
-  {
-    label: 'Github登录',
-    value: 'oauthGithub',
-    apply: 'https://github.com/settings/apps/new',
-  },
-  { label: 'Gitee登录', value: 'oauthGitee', apply: 'https://gitee.com/oauth/applications' },
   { label: '自定义Oauth', value: 'oauthCustom' },
 ]
 
